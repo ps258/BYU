22711d136e15e510f4b12afc4cb13f6f3ba1b95e
Peter Stubbs <ps258@hotmail.com>
2022-01-06 09:37:17 +0000

    plugin/build.sh changed to tabs

--- compose/plugin/build.sh
+++ compose/plugin/build.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 cd /root/plugin
-for bundle in register auth
+for bundle in auth register
 do
-  rm -f ${bundle}.zip
-  /opt/tyk-gateway/tyk bundle build -m ${bundle}.json -y -o ${bundle}.zip
-  if [[ ! -f ${bundle}.zip ]]
-  then
-    echo "[WARN]${bundle}.zip not built!"
-  else
-    zip -ur ${bundle}.zip vendor/ ./jwt-signing-key.pem
-  fi
+	rm -f ${bundle}.zip
+	/opt/tyk-gateway/tyk bundle build -m ${bundle}.json -y -o ${bundle}.zip
+	if [[ ! -f ${bundle}.zip ]]
+	then
+		echo "[WARN]${bundle}.zip not built!"
+	else
+		if [[ -d vendor ]]; then
+			zip -ur ${bundle}.zip vendor/ ./jwt-signing-key.pem
+		fi
+	fi
 done
 python3 -m http.server
